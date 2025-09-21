13fd0f5bf8c3130f1d3be5629528b06e4d04a766
Abhiram302 <abhiramburra3@gmail.com>
2025-09-21 15:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const button = document.querySelector("input[type = submit]")
+const size = document.getElementById("fontsize")
+const color = document.getElementById("fontcolor")
+button.addEventListener("click",()=>{
+	document.cookie = `fontsize=${size.value}px; path=/;`
+	document.cookie = `fontcolor=${color.value}; path=/;`
+	// document.documentElement.style.setProperty("--fontsize",size.value+"px")
+	// document.documentElement.style.setProperty("--fontcolor",color.value)
+})
+window.addEventListener("load",()=>{
+	let fs = getCookie("fontsize")
+	let fc = getCookie("fontcolor")
+	if(fs) document.documentElement.style.setProperty("--fontsize",fs)
+	if(fc) document.documentElement.style.setProperty("--fontcolor",fc)
+	size.value = parseInt(fs)
+	color.value = fc
+})
+
+function getCookie(cookieName){
+	let cookies = document.cookie.split(";")
+	let result = null
+	cookies.forEach((cookie)=>{
+		let trimmedCookie = cookie.trim()
+		if(trimmedCookie.includes(cookieName)){
+			result = trimmedCookie.substring(cookieName.length+1)
+		}
+	})
+	return result
+}
